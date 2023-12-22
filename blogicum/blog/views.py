0bc808c2dd19def2bdf83d5eb0aabad14553707c
Sergey Kuroedov <sergeylive87@yandex.ru>
2023-12-22 12:08:07 +0300

    Homework

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -145,9 +145,11 @@ class PostDetail(ObjectPostMixin, ModelFormMixin, DetailView):
     form_class = CommentForm
 
     def get(self, request, *args, **kwargs):
-        if (self.publish.is_published
+        if (
+            self.publish.is_published
             and self.publish.category.is_published
-            and self.publish.pub_date < timezone.now()):
+            and self.publish.pub_date < timezone.now()
+        ):
             return super().get(request, *args, *kwargs)
         else:
             if self.publish.author == request.user:
